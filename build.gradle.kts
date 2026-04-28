7d33bcd1d945e54e3c4f32fc09ab3a169c5b3a7f
koryakin.kirill9 <koryakin.kirill9@rwb.ru>
2026-04-28 23:43:15 +0700

    Allax изобрел мужчину и женщину валентина

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     kotlin("jvm") version "2.1.21"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
---
+++ src/test/kotlin/FormTest.kt
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.Test
+
+class FormTest {
+
+    @Test
+    fun main() {
+        // Целочисленные типы
+        val aByte: Byte = 0 // 8b   -128 + 127
+        val aShort: Short = 0 // 16b -32768 +32767
+        val aInt = 0 // 32b   (-2^32) .. (+2^32 -1)
+        val intWrapper = 0
+        val aLong = 0L // 64b
+        // Типы с плавающей точкой
+        val aFloat = 0.0f
+        val aDouble = 0.0
+        val doubleWrapper = 9.1
+        // Символьный
+        val aChar = 'a'
+        val charWrapper = 'a'
+        // Логически тип
+        val aBoolean = true
+        val booleanWrapper = true
+        // Строка (и бесконечность других объектных/ссылочных типов)
+        val toBePrint = "Hello world!"
+        val teachers = mutableListOf<Int?>(10, 20)
+
+        // Операторы
+        // Оператор присвоения =
+        val nameFirst = "Dmitrii"
+        val age = 34
+        val nameSecond = "Dmitrii"
+        // Арифметические операторы + - / * % ++ --
+        val result = aInt
+
+        // Операторы сравнения <, >, >=, <=, !=, ==
+
+        // Логические операторы &, |, &&, ||, !
+
+//    System.out.println(!(nameFirst.equals("Dmitrii")));
+
+        // Оператор instanceof
+            //println(nameFirst is String)
+
+        // Тернарный оператор
+        val sex = 'w'
+
+
+        var childName = if (sex == 'm')
+            "Valentin"
+        else
+            "Valentina"
+println(childName)
+        // Управляющая конструкция if
+        if (sex == 'm') {
+            childName = "Valentin"
+        } else if (sex == 'w') {
+            childName = "Valentina"
+        } else {
+            println("((")
+        }
+
+        // ключевое слово new
+        val name = "Dima"
+    }
+
+}
